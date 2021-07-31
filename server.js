f81484542f3a17acb4386b5bb1a255f818eea2d2
aakash-taneja <19uec168@lnmiit.ac.in>
2021-07-31 18:23:52 +0530

    added allowed clients in env

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const path = require('path');
 const cors = require('cors');
 // Cors 
 const corsOptions = {
-   origin: //process.env.ALLOWED_CLIENTS.split(',')
-  ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+   origin: process.env.ALLOWED_CLIENTS.split(',')
+  
 }
 
 // Default configuration looks like
